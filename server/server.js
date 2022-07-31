95fca37e076e9bd43334aad14461c40cac6ace31
Christopher Rios <chris12k9@yahoo.com>
2022-07-31 10:24:21 -0700

    server js into server

--- server/server.js
+++ server/server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-require('./server/config/mongoose.config'); 
+require('./config/mongoose.config'); 
 
 // Make sure these lines are above any app.get app.post lines
 app.use(cors());
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
 
-require('./server/routes/sfhard.routes')(app); // Change this line 
+require('./routes/sfhard.routes')(app); // Change this line 
 
 app.listen(8000, () => {
     console.log("Listening at Port 8000")
